77137b1dd02b3fe3dab42d0a5287bb75324e2412
Daxanius <balder.huybreghs@gmail.com>
2022-10-13 19:08:12 +0200

    Voeg toe DTO klassen

---
+++ BezoekersRegistratieSysteem/BezoekersRegistratieSysteemBL/DTO/DTOBezoeker.cs
@@ -0,0 +1,9 @@
+﻿namespace BezoekersRegistratieSysteemBL.DTO {
+	public class DTOBezoeker : DTOPersoon {
+		public DTOBezoeker(string voornaam, string achternaam, string email, uint bedrijfId) : base(voornaam, achternaam, email) {
+			BedrijfId = bedrijfId;
+		}
+
+		public uint BedrijfId { get; set; }
+	}
+}
---
+++ BezoekersRegistratieSysteem/BezoekersRegistratieSysteemBL/DTO/DTOParkingContract.cs
@@ -0,0 +1,13 @@
+﻿namespace BezoekersRegistratieSysteemBL.DTO {
+	public class DTOParkingContract {
+		public DTOParkingContract(DateTime starttijd, DateTime? eindtijd, int aantalPlaatsen) {
+			Starttijd = starttijd;
+			Eindtijd = eindtijd;
+			AantalPlaatsen = aantalPlaatsen;
+		}
+
+		public DateTime Starttijd { get; private set; }
+		public DateTime? Eindtijd { get; private set; }
+		public int AantalPlaatsen { get; private set; }
+	}
+}
