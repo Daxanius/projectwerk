7e023a7e69ea25bbc98052df82cd131c04a73333
Guillaume Desktop <robertohelper@outlook.com>
2022-10-18 18:29:02 +0200

    implemented BewerkBedrijf

--- BezoekersRegistratieSysteem/BezoekersRegistratieSysteemDL/BedrijfRepoADO.cs
+++ BezoekersRegistratieSysteem/BezoekersRegistratieSysteemDL/BedrijfRepoADO.cs
@@ -24,6 +24,7 @@ namespace BezoekersRegistratieSysteemDL {
         private SqlConnection GetConnection() {
             return new SqlConnection(_connectieString);
         }
+
         /// <summary>
         /// Methode die kijkt of bedrijf in DB bestaat adh bedrijf object
         /// </summary>
@@ -38,6 +39,12 @@ namespace BezoekersRegistratieSysteemDL {
             }
         }
 
+        /// <summary>
+        /// Methode die kijkt of bedrijf in DB bestaat adh bedrijf id
+        /// </summary>
+        /// <param name="bedrijfId"></param>
+        /// <returns>bool</returns>
+        /// <exception cref="BedrijfADOException"></exception>
         public bool BestaatBedrijf(uint bedrijfId) {
             try {
                 return BestaatBedrijf(null, bedrijfId, null);
@@ -46,6 +53,12 @@ namespace BezoekersRegistratieSysteemDL {
             }
         }
 
+        /// <summary>
+        /// Methode die kijkt of bedrijf in DB bestaat adh bedrijf naam
+        /// </summary>
+        /// <param name="bedrijfsnaam"></param>
+        /// <returns>bool</returns>
+        /// <exception cref="BedrijfADOException"></exception>
         public bool BestaatBedrijf(string bedrijfsnaam) {
             try {
                 return BestaatBedrijf(null, null, bedrijfsnaam);
@@ -54,6 +67,14 @@ namespace BezoekersRegistratieSysteemDL {
             }
         }
 
+        /// <summary>
+        /// Private methode die kijkt of bedrijf in DB bestaat adh bedrijf object, id, naam
+        /// </summary>
+        /// <param name="bedrijf"></param>
+        /// <param name="bedrijfId"></param>
+        /// <param name="bedrijfsnaam"></param>
+        /// <returns>bool</returns>
+        /// <exception cref="BedrijfADOException"></exception>
         private bool BestaatBedrijf(Bedrijf? bedrijf, uint? bedrijfId, string? bedrijfsnaam) {
             SqlConnection con = GetConnection();
             string query = "SELECT COUNT(*) " +
@@ -82,8 +103,45 @@ namespace BezoekersRegistratieSysteemDL {
             }
         }
 
+        /// <summary>
+        /// Bewerkt informatie van een bedrijf
+        /// </summary>
+        /// <param name="bedrijf"></param>
+        /// <exception cref="BedrijfADOException"></exception>
         public void BewerkBedrijf(Bedrijf bedrijf) {
-            throw new NotImplementedException();
+            SqlConnection con = GetConnection();
+            string query = "UPDATE bedrijf " +
+                           "SET Naam = @naam, " +
+                           "BTWNr = @btwnr, " +
+                           "TeleNr = @telenr, " +
+                           "Email = @email, " +
+                           "Adres = @adres " +
+                           "WHERE id = @id";
+            try {
+                using (SqlCommand cmd = con.CreateCommand()) {
+                    con.Open();
+                    cmd.CommandText = query;
+                    cmd.Parameters.Add(new SqlParameter("@naam",SqlDbType.VarChar));
+                    cmd.Parameters.Add(new SqlParameter("@btwnr", SqlDbType.VarChar));
+                    cmd.Parameters.Add(new SqlParameter("@telenr", SqlDbType.VarChar));
+                    cmd.Parameters.Add(new SqlParameter("@email", SqlDbType.VarChar));
+                    cmd.Parameters.Add(new SqlParameter("@adres", SqlDbType.VarChar));
+                    cmd.Parameters.Add(new SqlParameter("@id", SqlDbType.BigInt));
+                    cmd.Parameters["@naam"].Value = bedrijf.Naam;
+                    cmd.Parameters["@btwnr"].Value = bedrijf.BTW;
+                    cmd.Parameters["@telenr"].Value = bedrijf.TelefoonNummer;
+                    cmd.Parameters["@email"].Value = bedrijf.Email;
+                    cmd.Parameters["@adres"].Value = bedrijf.Adres;
+                    cmd.Parameters["@id"].Value = bedrijf.Id;
+                    cmd.ExecuteNonQuery();
+                }
+            } catch (Exception ex) {
+                BedrijfADOException exx = new BedrijfADOException($"BedrijfRepoADO: BewerkBedrijf {ex.Message}", ex);
+                exx.Data.Add("bedrijf", bedrijf);
+                throw exx;
+            } finally {
+                con.Close();
+            }
         }
 
         public Bedrijf GeefBedrijf(uint id) {
@@ -101,7 +159,9 @@ namespace BezoekersRegistratieSysteemDL {
         public void VerwijderBedrijf(uint id) {
             throw new NotImplementedException();
         }
-
+        private void VeranderStatusBedrijf(uint bedrijfId, int statusId) { 
+            
+        }
         public Bedrijf VoegBedrijfToe(Bedrijf bedrijf) {
             throw new NotImplementedException();
         }
