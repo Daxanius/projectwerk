489b9655eca3fba479606c601b30435e71bfee70
Guillaume Desktop <robertohelper@outlook.com>
2022-10-18 16:44:22 +0200

    Added base Bedrijf ADO

---
+++ BezoekersRegistratieSysteem/BezoekersRegistratieSysteemDL/BedrijfRepoADO.cs
@@ -0,0 +1,47 @@
+﻿using BezoekersRegistratieSysteemBL.Domeinen;
+using BezoekersRegistratieSysteemBL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezoekersRegistratieSysteemDL {
+    public class BedrijfRepoADO : IBedrijfRepository {
+        public bool BestaatBedrijf(Bedrijf bedrijf) {
+            throw new NotImplementedException();
+        }
+
+        public bool BestaatBedrijf(uint bedrijf) {
+            throw new NotImplementedException();
+        }
+
+        public bool BestaatBedrijf(string bedrijfsnaam) {
+            throw new NotImplementedException();
+        }
+
+        public void BewerkBedrijf(Bedrijf bedrijf) {
+            throw new NotImplementedException();
+        }
+
+        public Bedrijf GeefBedrijf(uint id) {
+            throw new NotImplementedException();
+        }
+
+        public Bedrijf GeefBedrijf(string bedrijfsnaam) {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<Bedrijf> Geefbedrijven() {
+            throw new NotImplementedException();
+        }
+
+        public void VerwijderBedrijf(uint id) {
+            throw new NotImplementedException();
+        }
+
+        public Bedrijf VoegBedrijfToe(Bedrijf bedrijf) {
+            throw new NotImplementedException();
+        }
+    }
+}
