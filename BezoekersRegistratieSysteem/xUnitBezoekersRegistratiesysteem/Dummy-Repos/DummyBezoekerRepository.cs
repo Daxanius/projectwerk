0d296e3111fdfa5bdfab241a97bbca214e83806b
Stan Persoons <Stan.persoons2@gmail.com>
2022-10-18 22:33:27 +0200

    Managers zijn afgewerkt behalve parkeer manager

---
+++ BezoekersRegistratieSysteem/xUnitBezoekersRegistratiesysteem/Dummy-Repos/DummyBezoekerRepository.cs
@@ -0,0 +1,77 @@
+﻿using BezoekersRegistratieSysteemBL.Domeinen;
+using BezoekersRegistratieSysteemBL.Exceptions.DomeinException;
+using BezoekersRegistratieSysteemBL.Interfaces;
+
+namespace xUnitBezoekersRegistratiesysteem.DummyData.Repos
+{
+	public class DummyBezoekerRepository : IBezoekerRepository
+	{
+		private readonly Dictionary<uint, Bezoeker> _bezoekers = new();
+		private readonly DummyAfspraakRepository _afspraakRepository = new();
+		private uint _lastId = 0;
+
+		public bool BestaatBezoeker(Bezoeker bezoeker)
+		{
+			return BestaatBezoeker(bezoeker.Id);
+		}
+
+		public bool BestaatBezoeker(uint bezoeker)
+		{
+			return _bezoekers.ContainsKey(bezoeker);
+		}
+
+		public IReadOnlyList<Bezoeker> GeefAanwezigeBezoekers()
+		{
+			var huidigeAfspraken = _afspraakRepository.GeefHuidigeAfspraken();
+
+			return huidigeAfspraken.Where(a => a.Eindtijd == null).Select(a => a.Bezoeker).ToList();
+		}
+
+		public Bezoeker GeefBezoeker(uint id)
+		{
+			if (!_bezoekers.ContainsKey(id))
+				throw new Exception("Bezoeker bestaat niet");
+			return _bezoekers[id];
+		}
+
+		public IReadOnlyList<Bezoeker> GeefBezoekerOpNaam(string naam, string achternaam)
+		{
+			return _bezoekers.Select(b => b.Value).Where(b => b.Voornaam.ToLower() == naam.ToLower() && b.Achternaam.ToLower() == achternaam.ToLower()).ToList();
+		}
+
+		public IReadOnlyList<Bezoeker> GeefBezoekersOpDatum(DateTime datum)
+		{
+			var huidigeAfspraken = _afspraakRepository.GeefHuidigeAfspraken();
+			return huidigeAfspraken.Where(a => a.Starttijd.Date == datum.Date).Select(a => a.Bezoeker).ToList();
+		}
+
+		public void VerwijderBezoeker(uint id)
+		{
+			if (!_bezoekers.ContainsKey(id))
+				throw new Exception("Bezoeker bestaat niet");
+			_bezoekers.Remove(id);
+		}
+
+		public void VoegBezoekerToe(Bezoeker bezoeker)
+		{
+			if (bezoeker == null)
+				throw new BezoekerException("bezoeker is null");
+			_bezoekers.Add(_lastId++, bezoeker);
+		}
+
+		public void WijzigBezoeker(Bezoeker bezoeker)
+		{
+			if (!_bezoekers.ContainsKey(bezoeker.Id))
+				throw new Exception("Bezoeker bestaat niet");
+			_bezoekers[bezoeker.Id] = bezoeker;
+		}
+
+		Bezoeker IBezoekerRepository.VoegBezoekerToe(Bezoeker bezoeker)
+		{
+			if (bezoeker == null)
+				throw new BezoekerException("bezoeker is null");
+			_bezoekers.Add(_lastId++, bezoeker);
+			return _bezoekers[_lastId];
+		}
+	}
+}
--- BezoekersRegistratieSysteem/xUnitBezoekersRegistratiesysteem/Managers/BedrijfsManagerTest.cs
+++ BezoekersRegistratieSysteem/xUnitBezoekersRegistratiesysteem/Managers/BedrijfsManagerTest.cs
@@ -1,89 +1,126 @@
-//using BezoekersRegistratieSysteemBL.Domeinen;
-//using BezoekersRegistratieSysteemBL.Exceptions.ManagerException;
-//using BezoekersRegistratieSysteemBL.Interfaces;
-//using xUnitBezoekersRegistratiesysteem.DummyData.Repos;
+using BezoekersRegistratieSysteemBL.Domeinen;
+using BezoekersRegistratieSysteemBL.Exceptions.ManagerException;
+using BezoekersRegistratieSysteemBL.Interfaces;
+using xUnitBezoekersRegistratiesysteem.DummyData.Repos;
 
-//namespace BezoekersRegistratieSysteemBL.Managers
-//{
-//	public class BedrijfsManagerTest
-//	{
-//		private readonly IBedrijfRepository _bedrijfRepository;
+namespace BezoekersRegistratieSysteemBL.Managers
+{
+	public class BedrijfsManagerTest
+	{
+		private readonly IBedrijfRepository _bedrijfRepository;
 
-//		private readonly Bezoeker validBezoeker;
-//		private readonly DateTime validStarttijd;
-//		private readonly DateTime validEindtijd;
-//		private readonly Afspraak validAfspraak;
-//		private readonly Werknemer validWerknemer;
-//		private readonly Bedrijf validBedrijf;
+		private readonly Bezoeker validBezoeker;
+		private readonly DateTime validStarttijd;
+		private readonly DateTime validEindtijd;
+		private readonly Afspraak validAfspraak;
+		private readonly Werknemer validWerknemer;
+		private readonly Bedrijf validBedrijf;
 
-//		public BedrijfsManagerTest(IBedrijfRepository bedrijfRepository)
-//		{
-//			this._bedrijfRepository = bedrijfRepository;
+		public BedrijfsManagerTest()
+		{
+			this._bedrijfRepository = new DummyBedrijfsRepository();
 
-//			validBezoeker = new(voornaam: "stan", achternaam: "persoons", email: "stan@gmail.com", bedrijf: "Artevelde");
-//			validStarttijd = DateTime.Now;
-//			validEindtijd = DateTime.Now.AddHours(8);
-//			validBedrijf = new(naam: "HoGent", btw: "BE0475730461", telefoonNummer: "0476687242", email: "mail@hogent.be", adres: "Kerkstraat snorkelland 9000 101");
-//			validWerknemer = new(voornaam: "wout", achternaam: "balding", email: "wout@gmail.com", bedrijf: validBedrijf, functie: "CEO");
-//			validAfspraak = new Afspraak(starttijd: validStarttijd, bezoeker: validBezoeker, werknemer: validWerknemer);
+			validBezoeker = new(voornaam: "stan", achternaam: "persoons", email: "stan@gmail.com", bedrijf: "Artevelde");
+			validStarttijd = DateTime.Now;
+			validEindtijd = DateTime.Now.AddHours(8);
+			validBedrijf = new(naam: "HoGent", btw: "BE0475730461", telefoonNummer: "0476687242", email: "mail@hogent.be", adres: "Kerkstraat snorkelland 9000 101");
+			validWerknemer = new(voornaam: "wout", achternaam: "balding", email: "wout@gmail.com");
+			validAfspraak = new Afspraak(starttijd: validStarttijd, bezoeker: validBezoeker, werknemer: validWerknemer);
 
-//			_bedrijfRepository = new DummyBedrijfsRepository();
-//		}
+			_bedrijfRepository = new DummyBedrijfsRepository();
 
-//		public void VoegBedrijfToe()
-//		{
-//			List<Bedrijf> bedrijven = _bedrijfRepository.GeefBedrijven().ToList();
+			validBedrijf.ZetId(1);
+			validWerknemer.ZetId(2);
+			validAfspraak.ZetId(3);
+			validBezoeker.ZetId(4);
 
-//			Assert.DoesNotContain(validBedrijf, bedrijven);
+			validWerknemer.VoegBedrijfEnFunctieToeAanWerknemer(validBedrijf, "CEO");
+		}
 
-//			_bedrijfRepository.VoegBedrijfToe(validBedrijf);
+		[Fact]
+		public void VoegBedrijfToe()
+		{
+			_bedrijfRepository.VerwijderBedrijf(validBedrijf.Id);
 
-//			bedrijven = _bedrijfRepository.GeefBedrijven().ToList();
+			List<Bedrijf> bedrijven = _bedrijfRepository.Geefbedrijven().ToList();
 
-//			Assert.Contains(validBedrijf, bedrijven);
-//		}
+			Assert.DoesNotContain(validBedrijf, bedrijven);
 
-//		public void VerwijderBedrijfById()
-//		{
-//			_bedrijfRepository.VoegBedrijfToe(validBedrijf);
+			_bedrijfRepository.VoegBedrijfToe(validBedrijf);
 
-//			List<Bedrijf> bedrijven = _bedrijfRepository.GeefBedrijven().ToList();
+			bedrijven = _bedrijfRepository.Geefbedrijven().ToList();
 
-//			Assert.Contains(validBedrijf, bedrijven);
+			Assert.Contains(validBedrijf, bedrijven);
+		}
 
-//			_bedrijfRepository.VerwijderBedrijf(validBedrijf.Id);
+		[Fact]
+		public void VerwijderBedrijfById()
+		{
+			List<Bedrijf> bedrijven = _bedrijfRepository.Geefbedrijven().ToList();
 
-//			bedrijven = _bedrijfRepository.GeefBedrijven().ToList();
+			Assert.Contains(validBedrijf, bedrijven);
 
-//			Assert.DoesNotContain(validBedrijf, bedrijven);
-//		}
+			_bedrijfRepository.VerwijderBedrijf(validBedrijf.Id);
 
-//		public void GeefBedrijfById()
-//		{
-//			_bedrijfRepository.VoegBedrijfToe(validBedrijf);
+			bedrijven = _bedrijfRepository.Geefbedrijven().ToList();
 
-//			List<Bedrijf> bedrijven = _bedrijfRepository.GeefBedrijven().ToList();
+			Assert.DoesNotContain(validBedrijf, bedrijven);
+		}
 
-//			Assert.Contains(validBedrijf, bedrijven);
+		[Fact]
+		public void GeefBedrijfById()
+		{
+			List<Bedrijf> bedrijven = _bedrijfRepository.Geefbedrijven().ToList();
 
-//			Bedrijf insertedBedrijf = _bedrijfRepository.GeefBedrijfById(validBedrijf.Id);
+			Assert.Contains(validBedrijf, bedrijven);
 
-//			Assert.Equal(validBedrijf, insertedBedrijf);
-//		}
+			Bedrijf insertedBedrijf = _bedrijfRepository.GeefBedrijf(validBedrijf.Id);
 
-//		public void WijzigBedrijf()
-//		{
-			
-//		}
+			Assert.Equal(validBedrijf, insertedBedrijf);
+		}
 
-//		public IEnumerable<Bedrijf> Geefbedrijven()
-//		{
+		[Fact]
+		public void WijzigBedrijf()
+		{
+			Bedrijf bedrijf = validBedrijf;
+			bedrijf.ZetAdres("Nieuw Addres");
+			bedrijf.ZetEmail("email@gmail.com");
+			bedrijf.ZetId(4);
+			bedrijf.ZetTelefoonNummer("04766927844");
 
-//		}
+			_bedrijfRepository.VoegBedrijfToe(bedrijf);
+			_bedrijfRepository.BewerkBedrijf(bedrijf);
 
-//		public Bedrijf GeefBedrijfOpNaam()
-//		{
+			Bedrijf insertedBedrijf = _bedrijfRepository.GeefBedrijf(bedrijf.Id);
 
-//		}
-//	}
-//}
+			Assert.Equal(bedrijf, insertedBedrijf);
+		}
+
+		[Fact]
+		public void Geefbedrijven()
+		{
+			List<Bedrijf> bedrijven = _bedrijfRepository.Geefbedrijven().ToList();
+			Assert.Contains(validBedrijf, bedrijven);
+		}
+
+		[Theory]
+		[InlineData("aNaam")]
+		[InlineData("bNaam")]
+		[InlineData("cNaam")]
+		public void GeefBedrijfOpNaam(string bedrijfsNaam)
+		{
+			Bedrijf bedrijf = _bedrijfRepository.GeefBedrijf(bedrijfsNaam);
+			Assert.Equal(bedrijfsNaam, bedrijf.Naam);
+		}
+
+		[Theory]
+		[InlineData(1)]
+		[InlineData(2)]
+		[InlineData(3)]
+		public void GeefBedrijfOpId(uint bedrijfsId)
+		{
+			Bedrijf bedrijf = _bedrijfRepository.GeefBedrijf(bedrijfsId);
+			Assert.Equal(bedrijfsId, bedrijf.Id);
+		}
+	}
+}
