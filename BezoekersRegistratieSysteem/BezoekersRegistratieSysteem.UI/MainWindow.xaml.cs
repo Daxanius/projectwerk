b35dc7e8568ee2646c11437e7d5032a1d8644e85
WDCHogent <91737565+WDCHogent@users.noreply.github.com>
2022-10-20 20:40:17 +0200

    UI and stuff 100

---
+++ BezoekersRegistratieSysteem/BezoekersRegistratieSysteem.UI/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BezoekersRegistratieSysteem.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ParkeerButton_Click(object sender, RoutedEventArgs e)
+        {
+            ParkeerWindow parkeerWindow = new ParkeerWindow();
+            this.Close();
+            parkeerWindow.ShowDialog();
+        }
+
+        private void AanmeldButton_Click(object sender, RoutedEventArgs e)
+        {
+            ParkeerWindow parkeerWindow = new ParkeerWindow();
+            this.Close();
+            parkeerWindow.ShowDialog();
+        }
+
+        private void AdminButton_Click(object sender, RoutedEventArgs e)
+        {
+            ParkeerWindow parkeerWindow = new ParkeerWindow();
+            this.Close();
+            parkeerWindow.ShowDialog();
+        }
+    }
+}
