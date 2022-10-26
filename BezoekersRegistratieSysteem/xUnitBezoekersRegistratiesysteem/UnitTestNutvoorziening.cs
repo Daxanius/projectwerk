cdef979af33dd5f0f28ca4b01ab01595ecac3faf
Daxanius <balder.huybreghs@gmail.com>
2022-10-26 18:52:29 +0200

    Clean usings

--- BezoekersRegistratieSysteem/xUnitBezoekersRegistratiesysteem/UnitTestNutvoorziening.cs
+++ BezoekersRegistratieSysteem/xUnitBezoekersRegistratiesysteem/UnitTestNutvoorziening.cs
@@ -1,15 +1,6 @@
 ﻿using BezoekersRegistratieSysteemBL;
 using BezoekersRegistratieSysteemBL.Domeinen;
 using BezoekersRegistratieSysteemBL.DTO;
-using BezoekersRegistratieSysteemBL.Exceptions.DomeinException;
-using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization.Metadata;
-using System.Threading.Tasks;
 
 namespace xUnitBezoekersRegistratiesysteem
 {
