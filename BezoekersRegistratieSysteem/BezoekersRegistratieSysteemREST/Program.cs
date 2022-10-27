5a6350d1a00db18f03e8edd7d463c30708e2d510
Daxanius <balder.huybreghs@gmail.com>
2022-10-27 11:33:57 +0200

    Add datalaag

--- BezoekersRegistratieSysteem/BezoekersRegistratieSysteemREST/Program.cs
+++ BezoekersRegistratieSysteem/BezoekersRegistratieSysteemREST/Program.cs
@@ -1,7 +1,18 @@
 using BezoekersRegistratieSysteemBL.Managers;
+using BezoekersRegistratieSysteemDL;
 using BezoekersRegistratieSysteemREST.Repo;
 
+const string ENV_SQL_CONNECTION = "SQL_CONNECTION_STRING";
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Het instellen van omgevingsvariabelen voor de connection string
+builder.Host.ConfigureAppConfiguration((hostingContext, configuration) => {
+	// Zorg dat je deze user secret instelt voordat je de applicatie start
+	// zie https://dotnetcoretutorials.com/2022/04/28/using-user-secrets-configuration-in-net/ voor
+	// meer informatie
+	configuration.AddUserSecrets("SQL_CONNECTIONSTRING");
+});
 
 // Add services to the container.
 
@@ -10,13 +21,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+string? connectionstring = Environment.GetEnvironmentVariable(ENV_SQL_CONNECTION);
+
+if (connectionstring is null) {
+	Console.WriteLine($"Omgevingsvariabele {ENV_SQL_CONNECTION} is niet ingesteld");
+	return;
+}
+
 // Alle managers als singleton toevoegen
 // dit omdat de API interract met de managers
 // WAARCHUWING: DE REPOS ZIJN TIJDELIJK, MOETEN VERVANGEN WORDEN DOOR DB
-var manager = new BedrijfRepo();
+var manager = new BedrijfRepoADO(connectionstring);
 BedrijfManager bedrijfManager = new(manager);
-AfspraakManager afspraakManager = new(new AfspraakRepo());
-WerknemerManager werknemerManager = new(new WerknemerRepo(), manager);
+AfspraakManager afspraakManager = new(new AfspraakRepoADO(connectionstring));
+WerknemerManager werknemerManager = new(new WerknemerRepoADO(connectionstring), manager);
 
 builder.Services.AddSingleton(bedrijfManager);
 builder.Services.AddSingleton(afspraakManager);
