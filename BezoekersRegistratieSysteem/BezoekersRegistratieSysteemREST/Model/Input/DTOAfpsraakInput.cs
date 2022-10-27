60c0ef73212c2cd0fe167b5e344503a5c1a33dcd
Daxanius <balder.huybreghs@gmail.com>
2022-10-27 14:35:25 +0200

    Update DTOAfpsraakInput.cs

--- BezoekersRegistratieSysteem/BezoekersRegistratieSysteemREST/Model/Input/DTOAfpsraakInput.cs
+++ BezoekersRegistratieSysteem/BezoekersRegistratieSysteemREST/Model/Input/DTOAfpsraakInput.cs
@@ -1,15 +1,15 @@
 ﻿namespace BezoekersRegistratieSysteemREST.Model {
 	public class DTOAfpsraakInput {
-		public DTOAfpsraakInput(DateTime starttijd, DateTime? eindtijd, uint werknemerId, uint bezoekerId) {
+		public DTOAfpsraakInput(DateTime starttijd, DateTime? eindtijd, DTOBezoekerInput bezoeker, uint werknemerId) {
 			Starttijd = starttijd;
 			Eindtijd = eindtijd;
 			WerknemerId = werknemerId;
-			BezoekerId = bezoekerId;
+			Bezoeker = bezoeker;
 		}
 
 		public DateTime Starttijd { get; set; }
 		public DateTime? Eindtijd { get; set; }
+		public DTOBezoekerInput Bezoeker { get; set; }
 		public uint WerknemerId { get; set; }
-		public uint BezoekerId { get; set; }
 	}
 }
