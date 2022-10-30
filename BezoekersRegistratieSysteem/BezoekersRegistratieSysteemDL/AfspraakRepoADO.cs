7c50233b63c71400922145d21f85401f28792233
Daxanius <balder.huybreghs@gmail.com>
2022-10-30 12:45:03 +0100

    Zorg dat BTW Valid controle alleen gebeurd bij het aanmaken van een nieuw bedrijf

--- BezoekersRegistratieSysteem/BezoekersRegistratieSysteemDL/AfspraakRepoADO.cs
+++ BezoekersRegistratieSysteem/BezoekersRegistratieSysteemDL/AfspraakRepoADO.cs
@@ -302,7 +302,9 @@ namespace BezoekersRegistratieSysteemDL
 						//functie portie
 						string functieNaam = (string)reader["FunctieNaam"];
 						Werknemer werknemer = new Werknemer(werknemerId, werknemerVNaam, werknemerANaam);
-						werknemer.VoegBedrijfEnFunctieToeAanWerknemer(new Bedrijf(bedrijfId, bedrijfNaam, bedrijfBTWNr, bedrijfTeleNr, bedrijfMail, bedrijfAdres), werknemerMail, functieNaam);
+
+						// TODO VOOR GEKKE BJORN: Vervang de true in constructor door statuscode voor BTW
+						werknemer.VoegBedrijfEnFunctieToeAanWerknemer(new Bedrijf(bedrijfId, bedrijfNaam, bedrijfBTWNr, true, bedrijfTeleNr, bedrijfMail, bedrijfAdres), werknemerMail, functieNaam);
 						afspraak = new Afspraak(afspraakId, start, eind, new Bezoeker(bezoekerId, bezoekerVnaam, bezoekerAnaam, bezoekerMail, bezoekerBedrijf), werknemer);
 					}
 					return afspraak;
@@ -446,7 +448,9 @@ namespace BezoekersRegistratieSysteemDL
 						//functie portie
 						string functieNaam = (string)reader["FunctieNaam"];
 						Werknemer werknemer = new Werknemer(werknemerId, werknemerVNaam, werknemerANaam);
-						werknemer.VoegBedrijfEnFunctieToeAanWerknemer(new Bedrijf(bedrijfId, bedrijfNaam, bedrijfBTWNr, bedrijfTeleNr, bedrijfMail, bedrijfAdres), werknemerMail, functieNaam);
+
+						// TODO VOOR GEKKE BJORN: vervang true door statuscode
+						werknemer.VoegBedrijfEnFunctieToeAanWerknemer(new Bedrijf(bedrijfId, bedrijfNaam, bedrijfBTWNr, true, bedrijfTeleNr, bedrijfMail, bedrijfAdres), werknemerMail, functieNaam);
 						afspraken.Add(new Afspraak(afspraakId, start, eind, new Bezoeker(bezoekerId, bezoekerVnaam, bezoekerAnaam, bezoekerMail, bezoekerBedrijf), werknemer));
 					}
 					return afspraken.AsReadOnly();
@@ -588,7 +592,9 @@ namespace BezoekersRegistratieSysteemDL
 						//functie portie
 						string functieNaam = (string)reader["FunctieNaam"];
 						Werknemer werknemer = new Werknemer(werknemerId, werknemerVNaam, werknemerANaam);
-						werknemer.VoegBedrijfEnFunctieToeAanWerknemer(new Bedrijf(bedrijfId, bedrijfNaam, bedrijfBTWNr, bedrijfTeleNr, bedrijfMail, bedrijfAdres), werknemerMail, functieNaam);
+
+						// TODO VOOR GEKKE BJORN: True vervangen door statuscode
+						werknemer.VoegBedrijfEnFunctieToeAanWerknemer(new Bedrijf(bedrijfId, bedrijfNaam, bedrijfBTWNr, true, bedrijfTeleNr, bedrijfMail, bedrijfAdres), werknemerMail, functieNaam);
 						afspraken.Add(new Afspraak(long.Parse(afspraakId.ToString()), start, eind, new Bezoeker(bezoekerId, bezoekerVnaam, bezoekerAnaam, bezoekerMail, bezoekerBedrijf), werknemer));
 					}
 					return afspraken.AsReadOnly();
--- BezoekersRegistratieSysteem/xUnitBezoekersRegistratiesysteem/Domeinen/UnitTestWerknemer.cs
+++ BezoekersRegistratieSysteem/xUnitBezoekersRegistratiesysteem/Domeinen/UnitTestWerknemer.cs
@@ -8,8 +8,8 @@ namespace xUnitBezoekersRegistratiesysteem.Domeinen
 		//AF
 
 		#region Valid Info
-		private Bedrijf _b1 = new(10, "bedrijf", "BE0676747521", "012345678", "bedrijf@email.com", "bedrijfstraat 10");
-		private Bedrijf _b2 = new(1, "anderbedrijf", "BE0724540609", "876543210", "anderbedrijf@email.com", "anderebedrijfstraat 10");
+		private Bedrijf _b1 = new(10, "bedrijf", "BE0676747521", true, "012345678", "bedrijf@email.com", "bedrijfstraat 10");
+		private Bedrijf _b2 = new(1, "anderbedrijf", "BE0724540609", true, "876543210", "anderbedrijf@email.com", "anderebedrijfstraat 10");
 		private string _of = "oudefunctie";
 		private string _nf = "nieuwefunctie";
 		private string _e = "werknemer.werknemersen@email.com";
