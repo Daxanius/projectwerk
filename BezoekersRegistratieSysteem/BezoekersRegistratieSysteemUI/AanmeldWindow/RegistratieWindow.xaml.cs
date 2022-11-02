61a6769f2d4a92f54d24792c9adf64d5e1daea4f
Stan Persoons <Stan.persoons2@gmail.com>
2022-11-02 14:13:37 +0100

    Update

--- BezoekersRegistratieSysteem/BezoekersRegistratieSysteemUI/AanmeldWindow/RegistratieWindow.xaml.cs
+++ BezoekersRegistratieSysteem/BezoekersRegistratieSysteemUI/AanmeldWindow/RegistratieWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BezoekersRegistratieSysteemUI.BeheerderWindowDTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,25 @@ namespace BezoekersRegistratieSysteemUI.AanmeldWindow {
 	/// Interaction logic for RegistratieWindow.xaml
 	/// </summary>
 	public partial class RegistratieWindow : Window {
+		#region Scaling
+		public double ScaleX { get; set; }
+		public double ScaleY { get; set; }
+		#endregion
+
 		public RegistratieWindow() {
+
+			double schermResolutieHeight = System.Windows.SystemParameters.MaximizedPrimaryScreenHeight;
+			double schermResolutieWidth = System.Windows.SystemParameters.MaximizedPrimaryScreenWidth;
+
+			double defaultResHeight = 1080.0;
+			double defaultResWidth = 1920.0;
+
+			double aspectratio = schermResolutieWidth / schermResolutieHeight;
+			double change = aspectratio > 2 ? 0.3 : aspectratio > 1.5 ? 0 : aspectratio > 1 ? -0.05 : -0.3;
+
+			ScaleX = (schermResolutieWidth / defaultResWidth);
+			ScaleY = (schermResolutieHeight / defaultResHeight) + change;
+
 			this.DataContext = this;
 			InitializeComponent();
 		}
