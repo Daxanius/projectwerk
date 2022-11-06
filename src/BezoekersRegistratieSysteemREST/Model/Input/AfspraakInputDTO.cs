349619f86c798a08993cf3e5460156dca1e51a2b
Daxanius <balder.huybreghs@gmail.com>
2022-11-06 13:19:07 +0100

    Update AfspraakInputDTO.cs

--- src/BezoekersRegistratieSysteemREST/Model/Input/AfspraakInputDTO.cs
+++ src/BezoekersRegistratieSysteemREST/Model/Input/AfspraakInputDTO.cs
@@ -3,8 +3,17 @@ using BezoekersRegistratieSysteemBL.Domeinen;
 
 namespace BezoekersRegistratieSysteemREST.Model
 {
+	/// <summary>
+	/// De DTO voor inkomende afspraak informatie
+	/// </summary>
 	public class AfspraakInputDTO
 	{
+		/// <summary>
+		/// Zet de DTO om naar de business variant
+		/// </summary>
+		/// <param name="werknemerManager">De werknemer manager</param>
+		/// <param name="bedrijfManager">De bedrijf manager</param>
+		/// <returns>De Business variant</returns>
 		public Afspraak NaarBusiness(WerknemerManager werknemerManager, BedrijfManager bedrijfManager)
 		{
 			Werknemer werknemer = werknemerManager.GeefWerknemer(WerknemerId);
@@ -13,6 +22,12 @@ namespace BezoekersRegistratieSysteemREST.Model
 			return new(DateTime.Now, bedrijf, bezoeker, werknemer);
 		}
 
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="bezoeker"></param>
+		/// <param name="werknemerId"></param>
+		/// <param name="bedrijfId"></param>
 		public AfspraakInputDTO(BezoekerInputDTO bezoeker, long werknemerId, long bedrijfId)
 		{
 			WerknemerId = werknemerId;
@@ -20,8 +35,19 @@ namespace BezoekersRegistratieSysteemREST.Model
 			BedrijfId = bedrijfId;
 		}
 
+		/// <summary>
+		/// De bezoeker van de afspraak.
+		/// </summary>
 		public BezoekerInputDTO Bezoeker { get; set; }
+
+		/// <summary>
+		/// De werknemer waarmee de afspraak is gemaakt.
+		/// </summary>
 		public long WerknemerId { get; set; }
+
+		/// <summary>
+		/// Het bedrijf waarbinnen de afspraak is gemaakt.
+		/// </summary>
 		public long BedrijfId { get; set; }
 	}
 }
