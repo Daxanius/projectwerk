bad6c385b0f8e3643ef1c4472077373e85b3b5e9
Guillaume Desktop <robertohelper@outlook.com>
2022-11-16 13:41:09 +0100

    added set methods

--- src/BezoekersRegistratieSysteemBL/Domeinen/StatusObject.cs
+++ src/BezoekersRegistratieSysteemBL/Domeinen/StatusObject.cs
@@ -9,15 +9,26 @@ namespace BezoekersRegistratieSysteemBL.Domeinen
 {
     public class StatusObject
     {
-        public string Statusnaam { get; set; }
-        public object Obj { get; set; }
+        public string Statusnaam { get; private set; }
+        public object Obj { get; private set; }
 
         public StatusObject(string statusnaam, object obj)
         {
-            Statusnaam = statusnaam;
+            ZetStatusNaam(statusnaam);
+            ZetObject(obj);
+        }
+        public void ZetObject(object obj) {
+            if (obj is null) {
+                throw new StatusObjectException("Object is null");
+            }
             Obj = obj;
         }
-
+        public void ZetStatusNaam(string statusNaam) {
+            if (String.IsNullOrWhiteSpace(statusNaam)) {
+                throw new StatusObjectException("statusnaam is leeg");
+            }
+            Statusnaam = statusNaam.Trim();
+        }
         public Werknemer GeefWerknemerObject()
         {
             if (Obj is Werknemer werknemer)
