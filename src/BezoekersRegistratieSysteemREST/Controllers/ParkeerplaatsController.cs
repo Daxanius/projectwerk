8f20ded0e7b91421992ff0c45ac7b488f0fb47bc
Daxanius <balder.huybreghs@gmail.com>
2022-11-17 13:35:11 +0100

    ParkeerplaatsController constructor

--- src/BezoekersRegistratieSysteemREST/Controllers/ParkeerplaatsController.cs
+++ src/BezoekersRegistratieSysteemREST/Controllers/ParkeerplaatsController.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BezoekersRegistratieSysteemBL.Managers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BezoekersRegistratieSysteemREST.Controllers {
+	/// <summary>
+	/// De parkeerplaats controller zorgt ervoor
+	/// dat wij parkeerplaatsen kunnen beheren via de API.
+	/// </summary>
 	[Route("api/[controller]")]
 	[ApiController]
 	public class ParkeerplaatsController : ControllerBase {
+		private readonly ParkeerplaatsManager _parkeerplaatsManager;
+		private readonly BedrijfManager _bedrijfManager;
+
+		/// <summary>
+		/// De constructor.
+		/// </summary>
+		/// <param name="parkeerplaatsManager"></param>
+		/// <param name="bedrijfManager"></param>
+		public ParkeerplaatsController(ParkeerplaatsManager parkeerplaatsManager, BedrijfManager bedrijfManager) {
+			_parkeerplaatsManager = parkeerplaatsManager;
+			_bedrijfManager = bedrijfManager;	
+		}
 	}
 }
