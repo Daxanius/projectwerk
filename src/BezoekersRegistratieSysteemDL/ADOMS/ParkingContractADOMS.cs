1d889b4b3d269aab1d688f03223325a8e1bd0cd2
Guillaume Desktop <robertohelper@outlook.com>
2022-11-17 17:26:50 +0100

    implemented interface

---
+++ src/BezoekersRegistratieSysteemDL/ADOMS/ParkingContractADOMS.cs
@@ -0,0 +1,31 @@
+﻿using BezoekersRegistratieSysteemBL.Domeinen;
+using BezoekersRegistratieSysteemBL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezoekersRegistratieSysteemDL.ADOMS {
+    public class ParkingContractADOMS : IParkingContractRepository {
+        public bool BestaatParkingContract(ParkingContract parkingContract) {
+            throw new NotImplementedException();
+        }
+
+        public void BewerkParkingContract(ParkingContract parkingContract) {
+            throw new NotImplementedException();
+        }
+
+        public ParkingContract GeefParkingContract(long id) {
+            throw new NotImplementedException();
+        }
+
+        public void VerwijderParkingContract(ParkingContract parkingContract) {
+            throw new NotImplementedException();
+        }
+
+        public void VoegParkingContractToe(ParkingContract parkingContract) {
+            throw new NotImplementedException();
+        }
+    }
+}
