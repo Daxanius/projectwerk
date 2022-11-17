3628afe95e5a62d5e0d03324ff06f359ea5cdb99
Guillaume Desktop <robertohelper@outlook.com>
2022-11-17 17:27:14 +0100

    implemented interface

--- src/BezoekersRegistratieSysteemDL/ADOMS/ParkeerPlaatsADOMS.cs
+++ src/BezoekersRegistratieSysteemDL/ADOMS/ParkeerPlaatsADOMS.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using BezoekersRegistratieSysteemBL.Domeinen;
+using BezoekersRegistratieSysteemBL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BezoekersRegistratieSysteemDL.ADOMS {
-    internal class ParkeerPlaatsADOMS {
+    internal class ParkeerPlaatsADOMS : IParkeerplaatsRepository {
+        public bool BestaatNummerplaat(string nummerplaat) {
+            throw new NotImplementedException();
+        }
+
+        public void CheckNummerplaatIn(Parkeerplaats parkeerplaats) {
+            throw new NotImplementedException();
+        }
+
+        public void CheckNummerplaatUit(string nummerplaat) {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<string> GeefNummerplatenPerBedrijf(Bedrijf bedrijf) {
+            throw new NotImplementedException();
+        }
     }
 }
