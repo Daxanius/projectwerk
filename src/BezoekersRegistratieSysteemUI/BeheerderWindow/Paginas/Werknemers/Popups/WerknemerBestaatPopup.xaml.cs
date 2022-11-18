6639b129be1ebeee7bcc8ea22c4fd11dbdb71623
WDCHogent <91737565+WDCHogent@users.noreply.github.com>
2022-11-18 21:46:44 +0100

    MessageBoxes

---
+++ src/BezoekersRegistratieSysteemUI/BeheerderWindow/Paginas/Werknemers/Popups/WerknemerBestaatPopup.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BezoekersRegistratieSysteemUI.BeheerderWindowPaginas.Werknemers.Popups
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class WerknemerBestaatPopup : UserControl
+    {
+        public WerknemerBestaatPopup()
+        {
+            InitializeComponent();
+        }
+
+        private void AnnulerenButton_Click(object sender, RoutedEventArgs e)
+        {
+            SluitOverlay();
+        }
+
+        private void BevestigenButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void SluitOverlay()
+        {
+        }
+    }
+}
