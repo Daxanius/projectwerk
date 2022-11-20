a5837fa5c304cced643ba71596092e91e4597822
Guillaume Desktop <robertohelper@outlook.com>
2022-11-20 15:18:09 +0100

    added construc for parkeerplaatsado

--- src/BezoekersRegistratieSysteemDL/ADOMS/ParkeerPlaatsADOMS.cs
+++ src/BezoekersRegistratieSysteemDL/ADOMS/ParkeerPlaatsADOMS.cs
@@ -8,6 +8,19 @@ using System.Threading.Tasks;
 
 namespace BezoekersRegistratieSysteemDL.ADOMS {
     internal class ParkeerPlaatsADOMS : IParkeerplaatsRepository {
+        /// <summary>
+		/// Private lokale variabele connectiestring
+		/// </summary>
+		private string _connectieString;
+
+        /// <summary>
+        /// ParkeerPlaatsADOMS constructor krijgt connectie string als parameter.
+        /// </summary>
+        /// <param name="connectieString">Connectie string database</param>
+        /// <remarks>Deze constructor stelt de lokale variabele [_connectieString] gelijk aan de connectie string parameter.</remarks>
+        public ParkeerPlaatsADOMS(string connectieString) {
+            _connectieString = connectieString;
+        }
         public bool BestaatNummerplaat(string nummerplaat) {
             throw new NotImplementedException();
         }
