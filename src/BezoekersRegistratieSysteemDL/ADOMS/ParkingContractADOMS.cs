77f036775d2b5c59b0c65c7679a841ea41821ba3
Guillaume Desktop <robertohelper@outlook.com>
2022-11-20 15:40:26 +0100

    added connection string and constructor in parkingcontract

--- src/BezoekersRegistratieSysteemDL/ADOMS/ParkingContractADOMS.cs
+++ src/BezoekersRegistratieSysteemDL/ADOMS/ParkingContractADOMS.cs
@@ -2,12 +2,34 @@
 using BezoekersRegistratieSysteemBL.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BezoekersRegistratieSysteemDL.ADOMS {
     public class ParkingContractADOMS : IParkingContractRepository {
+        /// <summary>
+        /// Private lokale variabele connectiestring
+        /// </summary>
+        private string _connectieString;
+
+        /// <summary>
+        /// ParkingContractADOMS constructor krijgt connectie string als parameter.
+        /// </summary>
+        /// <param name="connectieString">Connectie string database</param>
+        /// <remarks>Deze constructor stelt de lokale variabele [_connectieString] gelijk aan de connectie string parameter.</remarks>
+        public ParkingContractADOMS(string connectieString) {
+            _connectieString = connectieString;
+        }
+
+        /// <summary>
+        /// Zet SQL connectie op met desbetreffende database adv de lokale variabele [_connectieString].
+        /// </summary>
+        /// <returns>SQL connectie</returns>
+        private SqlConnection GetConnection() {
+            return new SqlConnection(_connectieString);
+        }
         public bool BestaatParkingContract(ParkingContract parkingContract) {
             throw new NotImplementedException();
         }
