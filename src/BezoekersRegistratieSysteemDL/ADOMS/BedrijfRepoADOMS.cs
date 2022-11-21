4849b96c29205e9d713626d7f8c0810f32cdc653
Guillaume Laptop <robertohelper@outlook.com>
2022-11-21 08:55:04 +0100

    Added status 'werkzaam' to employees when calling GeefBedrijf or GeefBedrijven

--- src/BezoekersRegistratieSysteemDL/ADOMS/BedrijfRepoADOMS.cs
+++ src/BezoekersRegistratieSysteemDL/ADOMS/BedrijfRepoADOMS.cs
@@ -191,7 +191,7 @@ namespace BezoekersRegistratieSysteemDL.ADOMS {
 			SqlConnection con = GetConnection();
 			string query = "SELECT b.Id as BedrijfId, b.Naam as BedrijfNaam, b.BTWNr as BedrijfBTW, b.TeleNr as BedrijfTeleNr, b.Email as BedrijfMail, b.Adres as BedrijfAdres, b.BTWChecked, " +
 						   "wn.Id as WerknemerId, wn.ANaam as WerknemerAnaam, wn.VNaam as WerknemerVNaam, wb.WerknemerEMail, " +
-						   "f.FunctieNaam " +
+                           "f.FunctieNaam " +
 						   "FROM Bedrijf b " +
 						   "LEFT JOIN WerknemerBedrijf wb ON(b.id = wb.BedrijfId) AND wb.Status = 1 " +
 						   "LEFT JOIN Werknemer wn ON(wn.id = wb.WerknemerId) " +
@@ -235,6 +235,10 @@ namespace BezoekersRegistratieSysteemDL.ADOMS {
 							string werknemerMail = (string)reader["WerknemerEMail"];
 							string functieNaam = (string)reader["FunctieNaam"];
 							bedrijf.VoegWerknemerToeInBedrijf(werknemer, werknemerMail, functieNaam);
+                            //Halen momenteel enkel werknemers op die werkzaam zijn
+                            //Voeg dit aan qeury toe 'wb.Status as WerknemerBedrijfStatus'
+                            //string werknemerbedrijfStatus = (int)reader["WerknemerBedrijfStatus"] == 1 ? "Werkzaam" : "Niet werkzaam";
+                            werknemer.ZetStatusNaamPerBedrijf(bedrijf, "Werkzaam");
 						}
 					}
 					return bedrijf;
@@ -296,6 +300,10 @@ namespace BezoekersRegistratieSysteemDL.ADOMS {
 							string functieNaam = (string)reader["FunctieNaam"];
 
 							bedrijf.VoegWerknemerToeInBedrijf(werknemer, werknemerMail, functieNaam);
+                            //Halen momenteel enkel werknemers op die werkzaam zijn
+                            //Voeg dit aan qeury toe 'wb.Status as WerknemerBedrijfStatus'
+                            //string werknemerbedrijfStatus = (int)reader["WerknemerBedrijfStatus"] == 1 ? "Werkzaam" : "Niet werkzaam";
+							werknemer.ZetStatusNaamPerBedrijf(bedrijf, "Werkzaam");
 						}
 					}
 					return bedrijven;
