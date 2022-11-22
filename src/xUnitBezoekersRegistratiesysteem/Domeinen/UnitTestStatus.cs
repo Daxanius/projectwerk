4517b4826ba789b400411d1c7ec8f3df53f6aebc
Daxanius <balder.huybreghs@gmail.com>
2022-11-22 17:19:58 +0100

    Verwijder usings

--- src/xUnitBezoekersRegistratiesysteem/Domeinen/UnitTestStatus.cs
+++ src/xUnitBezoekersRegistratiesysteem/Domeinen/UnitTestStatus.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace xUnitBezoekersRegistratieSysteem.Domeinen {
+﻿namespace xUnitBezoekersRegistratieSysteem.Domeinen {
 	public class UnitTestStatus {
 	}
 }
