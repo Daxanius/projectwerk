c61676855dd560317868d4c37cd15cfa03534556
Daxanius <balder.huybreghs@gmail.com>
2022-11-23 14:31:41 +0100

    ParkingContract VoegToe tests

--- src/xUnitBezoekersRegistratiesysteem/Managers/UnitTestParkingContractManager.cs
+++ src/xUnitBezoekersRegistratiesysteem/Managers/UnitTestParkingContractManager.cs
@@ -1,4 +1,5 @@
 ﻿using BezoekersRegistratieSysteemBL.Domeinen;
+using BezoekersRegistratieSysteemBL.Exceptions.ManagerException;
 using BezoekersRegistratieSysteemBL.Interfaces;
 using BezoekersRegistratieSysteemBL.Managers;
 using Moq;
@@ -12,7 +13,12 @@ namespace xUnitBezoekersRegistratieSysteem.Managers {
 
 		#region Valid Info
 		private readonly Werknemer _w;
-		private readonly Bedrijf _b;
+		private readonly Bedrijf _b1;
+		private readonly Bedrijf _b2;
+		private readonly ParkingContract _c;
+		private readonly int _p;
+		private readonly DateTime _st;
+		private readonly DateTime _et;
 		#endregion
 
 		#region Initialiseren
@@ -20,12 +26,48 @@ namespace xUnitBezoekersRegistratieSysteem.Managers {
 			_mockRepo = new();
 
 			_w = new(10, "werknemer", "werknemersen");
-			_b = new(10, "bedrijf", "BE0676747521", true, "012345678", "bedrijf@email.com", "bedrijfstraat 10");
+			_b1 = new(10, "bedrijf", "BE0676747521", true, "012345678", "bedrijf@email.com", "bedrijfstraat 10");
+			_b2 = new(20, "IKEA", "BE0676747521", true, "012345678", "ikea@sweden.furniture", "Stockholm 32");
 
-			_b.VoegWerknemerToeInBedrijf(_w, "werknemer.werknemerson@email.com", "werken");
+			_p = 10;
+
+			_st = DateTime.Now;
+			_et = _st.AddHours(2);
+
+			_b1.VoegWerknemerToeInBedrijf(_w, "werknemer.werknemerson@email.com", "werken");
+
+			_c = new(_b1, _st, _et, _p);
 
 			_parkingContractManager = new(_mockRepo.Object);
 		}
+		#endregion
+
+		#region UnitTest VoegParkingContractToe
+		[Fact]
+		public void VoegParkingContractToe_Leeg() {
+			Assert.Throws<ParkingContractManagerException>(() => {
+				_parkingContractManager.VoegParkingContractToe(null);
+			});
+		}
+
+		[Fact]
+		public void VoegParkingContractToe_Bestaat() {
+			_mockRepo.Setup(x => x.BestaatParkingContract(_c)).Returns(true);
+			Assert.Throws<ParkingContractManagerException>(() => {
+				_parkingContractManager.VoegParkingContractToe(_c);
+			});
+		}
+
+		[Fact]
+		public void VoegParkingContractToe_Overlappend() {
+			_mockRepo.Setup(x => x.BestaatParkingContract(_c)).Returns(true);
+			_mockRepo.Setup(x => x.GeefParkingContract(0)).Returns(_c);
+			ParkingContract overlappend = new(_b1, _st, _et, 12);
+			Assert.Throws<ParkingContractManagerException>(() => {
+				_parkingContractManager.VoegParkingContractToe(overlappend);
+			});
+		}
+
 		#endregion
 	}
 }
