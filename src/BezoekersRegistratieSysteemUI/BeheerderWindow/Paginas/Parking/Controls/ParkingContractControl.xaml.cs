40af7a0bce5a240efc5d6cbff3903d118caa760c
WDCHogent <91737565+WDCHogent@users.noreply.github.com>
2022-11-30 20:57:00 +0100

    Beheerder parking window

---
+++ src/BezoekersRegistratieSysteemUI/BeheerderWindow/Paginas/Parking/Controls/ParkingContractControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using BezoekersRegistratieSysteemUI.Api;
+using BezoekersRegistratieSysteemUI.Beheerder;
+using BezoekersRegistratieSysteemUI.Events;
+using BezoekersRegistratieSysteemUI.MessageBoxes;
+using BezoekersRegistratieSysteemUI.Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace BezoekersRegistratieSysteemUI.BeheerderWindowPaginas.Parking.Controls {
+	public partial class ParkingContractControl : UserControl, INotifyPropertyChanged {
+
+		public ParkingContractControl() {
+			this.DataContext = this;
+			InitializeComponent();
+
+			//Kijk of je kan rechts klikken om iets te doen
+			//NummerplaatLijst.ContextMenuOpening += (sender, args) => args.Handled = true;
+			//ContextMenu.ContextMenuClosing += (object sender, ContextMenuEventArgs e) => ContextMenu.DataContext = null;
+		}
+
+		#region ProppertyChanged
+		public event PropertyChangedEventHandler? PropertyChanged;
+		public void UpdatePropperty([CallerMemberName] string propertyName = null) {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+		#endregion ProppertyChanged
+	}
+}
