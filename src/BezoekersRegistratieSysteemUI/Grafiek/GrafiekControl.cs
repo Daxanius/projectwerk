8a45e1f2e620cbb323f0d6425fdf8895c1cf8269
Daxanius <balder.huybreghs@gmail.com>
2022-12-01 14:49:48 +0100

    Refactor & multiline support

--- src/BezoekersRegistratieSysteemUI/Grafiek/GrafiekControl.cs
+++ src/BezoekersRegistratieSysteemUI/Grafiek/GrafiekControl.cs
@@ -4,102 +4,163 @@ using System.Globalization;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace BezoekersRegistratieSysteemUI.Grafiek
 {
     public class GrafiekControl : Control
     {
-        public Dictionary<string, double> Waarden { get; set; } = new();
-        public Brush MileStroke { get; set; } = Brushes.LightGray;
-        public Brush Stroke { get; set; } = Brushes.DarkBlue;
+        /// <summary>
+        /// De waarden per kolom, steunt meerdere lijnen
+        /// </summary>
+        public Dictionary<string, GrafiekDataset> Lijnen { get; set; } = new();
+        public List<string> Kolommen { get; set; } = new();
+        public Brush Stroke { get; set; } = Brushes.LightGray;
+        public double StrokeThickness { get; set; } = 1;
+        public int StrokeDot { get; set; } = 10;
+        public int StrokeDotPadding { get; set; } = 5;
         public string Font { get; set; } = "Arial";
-        public double StrokeThickness { get; set; } = 2;
-        public double MileStrokeThickness { get; set; } = 1;
-        public int MileStrokeDot { get; set; } = 10;
-        public int MileStrokeDotPadding { get; set; } = 5;
-        public int ValueIncrement { get; set; } = 10;
+        public int WaardeIncrement { get; set; } = 10;
         public double TextPadding { get; set; } = 10;
         public double PixelsPerDip { get; set; } = 10;
 
-        protected override void OnRender(DrawingContext drawingContext) {
-            base.OnRender(drawingContext);
-            Pen pen = new(Stroke, StrokeThickness);
-            var max = Waarden.Values.Max();
+        /// <summary>
+        /// Tekent een achtergrond gebaseerd op de grootste dataset
+        /// </summary>
+        /// <param name="drawingContext"></param>
+        private void TekenAchtergrond(DrawingContext drawingContext) {
+            // Haalt de grootste set op
+            int langsteSet = Lijnen.Values.Max(s => s.Data.Count);
 
-			List<Point> punten = new();
-            foreach (var key in Waarden.Keys) {
-                var width = punten.Count * (Width / (Waarden.Keys.Count-1));
-				punten.Add(new(width, Height - (Waarden[key] / max * Height * 0.9)));
-				drawingContext.DrawText(new(
-                        key, 
-                        CultureInfo.CurrentCulture, 
-                        FlowDirection.LeftToRight, 
-                        new(Font), 
-                        FontSize, 
-                        Foreground, 
-                        PixelsPerDip), 
-                    new(width - ((key.Length / 2) * FontSize) + PixelsPerDip / 2, Height + TextPadding));
-			}
+			for (int i = 0; i < langsteSet; i++) {
+                // X positie berekenen
+                double x = i * (Width / (langsteSet - 1));
 
-			for (int i = 0; i < punten.Count; i++) {
-				for (int ii = 0; ii < Height; ii += (MileStrokeDot + MileStrokeDotPadding)) {
-					drawingContext.DrawLine(new(MileStroke, MileStrokeThickness), new(punten[i].X, ii), new(punten[i].X, ii + MileStrokeDot));
+				// Verticale dotted lijn
+				for (int ii = 0; ii < Height; ii += (StrokeDot + StrokeDotPadding)) {
+					drawingContext.DrawLine(new(Stroke, StrokeThickness), new(x, ii), new(x, ii + StrokeDot));
 				}
 
-                if (i >= punten.Count - 1) {
-                    continue;
-                }
-
-				drawingContext.DrawLine(pen, punten[i], punten[i+1]);
-			}
-
-			for (int i = 0; i <= max; i += ValueIncrement) {
-                var textHeight = Height - (i / max * Height * 0.9);
-
-                //for (int ii = 0; ii < Width; ii += (MileStrokeDot + MileStrokeDotPadding))
-                //{
-                //    drawingContext.DrawLine(new(MileStroke, MileStrokeThickness), new(ii, height), new(ii + MileStrokeDot, height));
-                //}
-
-                drawingContext.DrawText(new(
-                        i.ToString(), 
-                        CultureInfo.CurrentCulture, 
-                        FlowDirection.RightToLeft,
-						new(Font), 
-                        FontSize, 
-                        Foreground,
-                        PixelsPerDip), 
-                    new(-TextPadding, textHeight));
+                // Horizontale dotted lijn
+				//for (int ii = 0; ii < Width; ii += (MileStrokeDot + MileStrokeDotPadding))
+				//{
+				//    drawingContext.DrawLine(new(MileStroke, MileStrokeThickness), new(ii, height), new(ii + MileStrokeDot, height));
+				//}
 			}
 		}
 
-        public GrafiekControl() {
-            Waarden.Add("1u", 30);
-            Waarden.Add("2u", 100);
-            Waarden.Add("3u", 34);
-            Waarden.Add("4u", 67);
-            Waarden.Add("5u", 54);
-            Waarden.Add("6u", 3);
-            Waarden.Add("7u", 0);
-            Waarden.Add("8u", 30);
-            Waarden.Add("9u", 100);
-            Waarden.Add("10u", 34);
-            Waarden.Add("11u", 67);
-            Waarden.Add("12u", 54);
-            Waarden.Add("13u", 3);
-            Waarden.Add("14u", 0);
-            Waarden.Add("15u", 30);
-            Waarden.Add("16u", 100);
-            Waarden.Add("17u", 34);
-            Waarden.Add("18u", 67);
-            Waarden.Add("19u", 54);
-            Waarden.Add("20u", 3);
-            Waarden.Add("21u", 0);
-            Waarden.Add("22u", 0);
-            Waarden.Add("23u", 0);
-            Waarden.Add("24u", 0);
+        /// <summary>
+        /// Tekent een dataset op de grafiek
+        /// </summary>
+        /// <param name="drawingContext"></param>
+        /// <param name="dataset"></param>
+        private void TekenDatasets(DrawingContext drawingContext) {
+            // Haalt de hoogste waarde op
+            double maxWaarde = Lijnen.Values.Max(x => x.Data.Max());
+			int langsteSet = Lijnen.Values.Max(s => s.Data.Count);
+
+			// We gaan door alle datasets gaan en ze individueel tekenen
+			foreach (var key in Lijnen.Keys) {
+                for (int i = 0; i < Lijnen[key].Data.Count -1; i++) {
+                    Point punt = new(i * (Width / (langsteSet - 1)), 
+                        Height - (Lijnen[key].Data[i] / maxWaarde * Height * 0.9));
+                    Point puntNext = new((i + 1) * (Width / (langsteSet - 1)), 
+                        Height - (Lijnen[key].Data[i + 1] / maxWaarde * Height * 0.9));
+
+                    drawingContext.DrawLine(Lijnen[key].GeefPen(), punt, puntNext);
+				}
+			}
         }
+
+        /// <summary>
+        /// Tekent de textuele informatie
+        /// </summary>
+        /// <param name="drawingContext"></param>
+        private void TekenInfo(DrawingContext drawingContext) {
+			// Haalt de hoogste waarden op
+			double maxWaarde = Lijnen.Values.Max(x => x.Data.Max());
+			int langsteSet = Lijnen.Values.Max(s => s.Data.Count);
+
+			// Tekent de onderste legende
+			for (int i = 0; i < Kolommen.Count && i < langsteSet; i++) {
+				var width = i * (Width / (langsteSet - 1));
+				drawingContext.DrawText(new(
+						Kolommen[i],
+						CultureInfo.CurrentCulture,
+						FlowDirection.LeftToRight,
+						new(Font),
+						FontSize,
+						Foreground,
+				PixelsPerDip),
+					new(width - ((Kolommen[i].Length / 2) * FontSize) + PixelsPerDip / 2, Height + TextPadding));
+			}
+
+			// Tekent de nummers met een increment
+			for (int i = 0; i <= maxWaarde; i += WaardeIncrement) {
+				var textHeight = Height - (i / maxWaarde * Height * 0.9);
+
+				drawingContext.DrawText(new(
+						i.ToString(),
+						CultureInfo.CurrentCulture,
+						FlowDirection.RightToLeft,
+						new(Font),
+						FontSize,
+						Foreground,
+						PixelsPerDip),
+					new(-TextPadding, textHeight));
+			}
+		}
+
+        protected override void OnRender(DrawingContext drawingContext) {
+            base.OnRender(drawingContext);
+            TekenAchtergrond(drawingContext);
+            TekenDatasets(drawingContext);
+            TekenInfo(drawingContext);
+		}
+
+        public GrafiekControl() {
+            // Een eerste dataset definieren
+			GrafiekDataset ds = new() {
+				Data = new() {
+					23,
+					34,
+					65,
+					65,
+					12,
+					34,
+					54
+				}
+			};
+
+			GrafiekDataset ds1 = new() {
+				Data = new() {
+					65,
+					12,
+					98,
+					34,
+					45,
+					8,
+					76
+				},
+				Stroke = Brushes.OrangeRed
+			};
+
+			// Kolommen definieren
+			Kolommen = new() {
+                "Ma",
+                "Di",
+                "Wo",
+                "Do",
+                "Vr",
+                "Za",
+                "Zo"
+            };
+
+            // De dataset toevoegen
+			Lijnen.Add("Gemiddeld", ds);
+			Lijnen.Add("Totaal ofz", ds1);
+		}
 
         static GrafiekControl()
         {
