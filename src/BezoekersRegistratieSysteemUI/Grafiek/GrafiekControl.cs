2ecc73ac230494c087c69728ea7befb4191fb727
Daxanius <balder.huybreghs@gmail.com>
2022-12-01 17:04:16 +0100

    Verwijder imports

--- src/BezoekersRegistratieSysteemUI/Grafiek/GrafiekControl.cs
+++ src/BezoekersRegistratieSysteemUI/Grafiek/GrafiekControl.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Media;
 
 namespace BezoekersRegistratieSysteemUI.Grafiek
